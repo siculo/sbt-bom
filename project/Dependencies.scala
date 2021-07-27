8cb3c40abd274b1b41e6c5108032eb527236fb1d
Fabrizio Di Giuseppe <fabrizio.digiuseppe@cerved.com>
2021-07-27 17:03:16 +0200

    wip: readme

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,6 +3,6 @@ import sbt._
 object Dependencies {
   lazy val library = Seq(
     "org.cyclonedx" % "cyclonedx-core-java" % "1.1.2",
-    "org.scalatest" %% "scalatest" % "3.0.5" % Test,
+    "org.scalatest" %% "scalatest" % "3.0.5" % Test
   )
 }
