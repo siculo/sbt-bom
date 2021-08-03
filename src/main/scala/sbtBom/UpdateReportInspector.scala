78a06fe6b3e445b00aad4dccf6102668436a12da
Fabrizio Di Giuseppe <fabrizio.digiuseppe@cerved.com>
2021-08-03 12:52:46 +0200

    fixed some method names in UpdateReportInspector

--- src/main/scala/sbtBom/UpdateReportInspector.scala
+++ src/main/scala/sbtBom/UpdateReportInspector.scala
@@ -8,33 +8,33 @@ class UpdateReportInspector(report: UpdateReport) {
   private val unlicensed = Seq(License(id = Some("Unlicense")))
 
   def dependencies(configuration: Configuration): Dependencies = {
-    val dependencies = report.configuration(configuration).map(createDependencies).getOrElse(Seq[Dependency]())
+    val dependencies = report.configuration(configuration).map(mapDependencies).getOrElse(Seq[Dependency]())
     Dependencies(dependencies)
   }
 
-  private def createDependencies(configurationReport: ConfigurationReport): Seq[Dependency] = {
-    configurationReport.modules.map(createDependency)
+  private def mapDependencies(configurationReport: ConfigurationReport): Seq[Dependency] = {
+    configurationReport.modules.map(mapDependency)
   }
 
-  private def createDependency(moduleReport: ModuleReport): Dependency = {
+  private def mapDependency(moduleReport: ModuleReport): Dependency = {
     Dependency(
       moduleReport.module.organization,
       moduleReport.module.name,
       moduleReport.module.revision,
       modified = false,
-      licenses = createLicenses(moduleReport.licenses),
+      licenses = mapLicenses(moduleReport.licenses),
       file = None
     )
   }
 
-  private def createLicenses(licenses: Seq[(String, Option[String])]): Seq[License] = {
+  private def mapLicenses(licenses: Seq[(String, Option[String])]): Seq[License] = {
     if (licenses.isEmpty)
       unlicensed
     else
-      licenses.map(createLicense)
+      licenses.map(mapLicense)
   }
 
-  private def createLicense(license: (String, Option[String])): License = {
+  private def mapLicense(license: (String, Option[String])): License = {
     license match {
       case (licenseName, licenseUrl) =>
         val licenseId: Option[String] = licenseUrl
